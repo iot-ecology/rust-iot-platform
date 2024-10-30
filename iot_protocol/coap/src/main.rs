ba745be984cf85fa3cb224853ab65e96cf1ee665
Zen Huifer <huifer97@163.com>
2024-10-30 18:25:03 +0800

    fix: coap

--- iot_protocol/coap/src/main.rs
+++ iot_protocol/coap/src/main.rs
@@ -1,3 +1,38 @@
+extern crate coap;
+
+use coap::Server;
+use coap_lite::{CoapRequest, RequestType as Method};
+use std::net::SocketAddr;
+use tokio::runtime::Runtime;
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:5683";
+    Runtime::new().unwrap().block_on(async move {
+        let mut server = Server::new_udp(addr).unwrap();
+        println!("Server up on {}", addr);
+
+        server
+            .run(|mut request: Box<CoapRequest<SocketAddr>>| async {
+                match request.get_method() {
+                    &Method::Get => println!("request by get {}", request.get_path()),
+                    &Method::Post => println!(
+                        "request by post {}",
+                        String::from_utf8(request.message.payload.clone()).unwrap()
+                    ),
+                    &Method::Put => println!(
+                        "request by put {}",
+                        String::from_utf8(request.message.payload.clone()).unwrap()
+                    ),
+                    _ => println!("request by other method"),
+                };
+                match request.response {
+                    Some(ref mut message) => {
+                        message.message.payload = b"OK".to_vec();
+                    }
+                    _ => {}
+                };
+                return request;
+            })
+            .await
+            .unwrap();
+    });
 }
