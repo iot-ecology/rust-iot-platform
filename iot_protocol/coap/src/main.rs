c7e5d9cfd4299029400e58bfe0aa7c327f5f0fdb
Zen Huifer <huifer97@163.com>
2024-10-31 20:27:11 +0800

    fix: coap server

--- iot_protocol/coap/src/main.rs
+++ iot_protocol/coap/src/main.rs
@@ -1,121 +1,143 @@
-extern crate coap;
-
 use coap::Server;
 use coap_lite::{CoapRequest, RequestType as Method};
 use common_lib::models::{Auth, CoapMessage};
 use common_lib::protocol_config::read_config;
-use common_lib::rabbit_utils::init_rabbitmq_with_config;
+use common_lib::rabbit_utils::{get_rabbitmq_instance, init_rabbitmq_with_config};
 use common_lib::redis_handler::{get_redis_instance, init_redis, RedisWrapper};
-use log::info;
+use log::{debug, info};
 use serde::{Deserialize, Serialize};
 use serde_json::from_str;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use tokio::runtime::Runtime;
+use tokio::sync::Mutex;
 use tokio::sync::MutexGuard;
 
 fn init_logger() {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 }
 
-#[tokio::main]
-async fn main() {
-    init_logger();
-    let result = read_config("app-local.yml").unwrap();
-    init_rabbitmq_with_config(result.mq_config).await.unwrap();
-    init_redis(result.redis_config).await.unwrap();
+fn main() {
     let addr = "127.0.0.1:5683";
+    let runtime = Runtime::new().unwrap();
 
-    let mut server = Server::new_udp(addr).unwrap();
-    info!("Server up on {}", addr);
+    init_logger();
+    let rt = Runtime::new().unwrap(); // 创建异步运行时
+    let result = read_config("app-local.yml").unwrap();
+    let node_info_name = result.node_info.name.clone(); // 克隆 result.node_info.name
+    rt.block_on(init_redis(result.redis_config)).unwrap(); // 初始化 Redis
+    rt.block_on(init_rabbitmq_with_config(result.mq_config))
+        .unwrap(); // 初始化 RabbitMQ
 
-    let node_name = result.node_info.name.clone();
+    runtime.block_on(async move {
+        let mut server = Server::new_udp(addr).unwrap();
+        println!("Server up on {}", addr);
 
-    server
-        .run(move |mut request: Box<CoapRequest<SocketAddr>>| {
-            let path = request.get_path().to_string();
-            let name = node_name.clone();
+        server
+            .run(move |request: Box<CoapRequest<SocketAddr>>| {
+                let request = Arc::new(Mutex::new(request));
+                let node_info_name = node_info_name.clone();
 
-            async move {
-                match request.get_method() {
-                    &Method::Get => handle_get_request(&path, &mut request, &name).await,
-                    &Method::Post => handle_post_request(&path, &request).await,
-                    _ => info!("Request to unknown path or unsupported method"),
+                async move {
+                    let mut request = request.lock().await; // 获取锁
+                    let method = request.get_method();
+
+                    let payload = String::from_utf8(request.message.payload.clone()).unwrap();
+
+                    let remote_address = request.source.unwrap().to_string();
+
+                    let rel_adds = remote_address.replace(":", "@");
+
+                    match method {
+                        Method::Get => {
+                            println!("Received GET request for path: {}", request.get_path());
+                            match request.get_path().as_str() {
+                                "auth" => {
+                                    let x = handle_auth(payload, node_info_name, rel_adds).await;
+
+                                    if let Some(ref mut message) = request.response {
+                                        message.message.payload = x.to_string().into();
+                                    }
+                                }
+                                "data" => {
+                                    let x = handle_data(payload, node_info_name, rel_adds).await;
+                                    if let Some(ref mut message) = request.response {
+                                        message.message.payload = x.to_string().into();
+                                    }
+                                }
+                                "/other" => handle_other(request.clone()).await,
+                                _ => {
+                                    if let Some(ref mut message) = request.response {
+                                        message.message.payload = b"404 Not Found".to_vec();
+                                    }
+                                }
+                            }
+                        }
+                        Method::Post => {
+                            let payload = String::from_utf8_lossy(&request.message.payload);
+                            println!("Received POST request with payload: {}", payload);
+                            if let Some(ref mut message) = request.response {
+                                message.message.payload = b"POST response".to_vec();
+                            }
+                        }
+                        Method::Put => {
+                            let payload = String::from_utf8_lossy(&request.message.payload);
+                            println!("Received PUT request with payload: {}", payload);
+                            if let Some(ref mut message) = request.response {
+                                message.message.payload = b"PUT response".to_vec();
+                            }
+                        }
+                        _ => {
+                            println!("Received request with unsupported method");
+                            if let Some(ref mut message) = request.response {
+                                message.message.payload = b"Method Not Allowed".to_vec();
+                            }
+                        }
+                    };
+
+                    request.clone() // 返回请求
                 }
-
-                request
-            }
-        })
-        .await
-        .unwrap();
+            })
+            .await
+            .unwrap();
+    });
 }
-/// 认证信息和设备结构体
 #[derive(Serialize, Deserialize, Debug)]
 struct AuthAndDevice {
     username: String,
     password: String,
     device_id: Option<String>,
 }
+// 处理 /auth 路径的函数
+async fn handle_auth(payload: String, name: String, rel_adds: String) -> &'static str {
+    // 处理认证逻辑
+    println!("Handling authentication logic.");
 
-async fn handle_get_request(path: &str, request: &mut CoapRequest<SocketAddr>, name: &str) {
-    let payload = String::from_utf8(request.message.payload.clone()).unwrap();
-    let remote_address = request.source.unwrap().to_string();
     let redis = get_redis_instance().await.unwrap();
-    let string1 = remote_address.replace(":", "@");
 
-    // let option = get_uid(name, string1.clone()).await.unwrap_or(String::new());
+    info!("auth");
+    let auth_device: Result<AuthAndDevice, _> = from_str(&payload);
+    return match auth_device {
+        Ok(auth_data) => {
+            let device_id = auth_data.device_id.unwrap();
+            let id = device_id.as_str();
 
-    // info!("uid = {}",option);
+            let x = find_device_mapping_up(redis, id.clone())
+                .await
+                .unwrap_or((String::new(), String::new()));
+            if x.0 == auth_data.username && x.1 == auth_data.password {
+                store_uid(name.as_str(), id.clone(), rel_adds).await;
 
-    if path == "auth" {
-        info!("auth");
-        let auth_device: Result<AuthAndDevice, _> = from_str(&payload);
-        match auth_device {
-            Ok(auth_data) => {
-                let device_id = auth_data.device_id.unwrap();
-                let id = device_id.as_str();
-
-                let x = find_device_mapping_up(redis, id.clone())
-                    .await
-                    .unwrap_or((String::new(), String::new()));
-                if x.0 == auth_data.username && x.1 == auth_data.password {
-                    store_uid(name, id.clone(), string1).await;
-
-                    if let Some(ref mut message) = request.response {
-                        message.message.payload = "认证通过".into();
-                    }
-                } else {
-                    if let Some(ref mut message) = request.response {
-                        message.message.payload = "认证失败".into();
-                    }
-                }
-
-                info!("auth 处理结束")
-            }
-            Err(e) => {
-                info!("Failed to parse payload as AuthAndDevice: {}", e);
+                "认证通过"
+            } else {
+                "认证失败"
             }
         }
-    } else if path == "data" {
-        info!("datadata");
-
-        info!("source ={}", string1);
-
-        if let Some(ref mut message) = request.response {
-            message.message.payload = "数据处理完成".into();
+        Err(e) => {
+            info!("Failed to parse payload as AuthAndDevice: {}", e);
+            "认证失败"
         }
-        info!("data 处理结束")
-    } else {
-        info!("Unknown GET request path")
-    }
-}
-async fn handle_post_request(path: &str, request: &CoapRequest<SocketAddr>) {
-    let payload = String::from_utf8(request.message.payload.clone()).unwrap();
-    let redis = get_redis_instance().await.unwrap();
-    match path {
-        "auth" => info!("POST request to /auth with payload: {}", payload),
-        "data" => info!("POST request to /data with payload: {}", payload),
-        _ => info!("Unknown POST request path"),
-    }
+    };
 }
 
 /// 在 Redis 中查找设备映射
@@ -139,13 +161,6 @@ async fn find_device_mapping_up(
         None
     }
 }
-
-async fn get_uid(name: &str, remote_address: String) -> Option<String> {
-    let redis = get_redis_instance().await.unwrap();
-    let key = format!("coap_uid_f:{}", name);
-    redis.get_hash(&key, remote_address.as_str()).await.unwrap()
-}
-
 async fn store_uid(name: &str, device_id: &str, remote_address: String) {
     let k1 = format!("coap_uid:{}", name);
     let k2 = format!("coap_uid_f:{}", name);
@@ -158,4 +173,43 @@ async fn store_uid(name: &str, device_id: &str, remote_address: String) {
         .set_hash(&k2, &remote_address, device_id)
         .await
         .expect("Failed to set hash");
+}
+
+// 处理 /data 路径的函数
+async fn handle_data(payload: String, name: String, rel_adds: String) -> &'static str {
+    let option = get_uid(name.as_str(), rel_adds).await;
+    if option.is_none() {
+        return "处理失败";
+    } else {
+        let msg = CoapMessage {
+            uid: option.unwrap(),
+            message: payload,
+        };
+        let json_data = msg.to_json_string();
+        debug!("Pushing message to queue: {}", json_data);
+
+        // 获取 RabbitMQ 实例并发布消息
+        let rabbit = get_rabbitmq_instance().await.unwrap();
+
+        rabbit
+            .publish("", "pre_coap_handler", json_data.as_str())
+            .await
+            .expect("publish message failed");
+
+        return "处理成功";
+    }
+}
+async fn get_uid(name: &str, remote_address: String) -> Option<String> {
+    let redis = get_redis_instance().await.unwrap();
+    let key = format!("coap_uid_f:{}", name);
+    redis.get_hash(&key, remote_address.as_str()).await.unwrap()
+}
+// 处理 /other 路径的函数
+async fn handle_other(mut request: Box<CoapRequest<SocketAddr>>) {
+    // 处理其他逻辑
+    println!("Handling other logic.");
+    // 返回响应
+    if let Some(ref mut message) = request.response {
+        message.message.payload = b"Other response".to_vec();
+    }
 }
